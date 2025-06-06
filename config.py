7805c002faba97a2a15c166c23642d8e0f580054
LeoNarcisoDev <leoonarciso@outlook.com>
2025-06-06 14:19:33 -0300

    banco de dados postgree render

--- config.py
+++ config.py
@@ -14,11 +14,11 @@ os.makedirs(os.path.join(BASE_DIR, 'static'), exist_ok=True)
 load_dotenv()
 
 DB_CONFIG = {
-    'host': os.getenv('POSTGRES_HOST'),
-    'port': os.getenv('POSTGRES_PORT'),
-    'dbname': os.getenv('POSTGRES_DB'),
-    'user': os.getenv('POSTGRES_USER'),
-    'password': os.getenv('POSTGRES_PASSWORD')
+    'dbname': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT', 5432),
 }
 
 UPLOAD_FOLDER = 'modelos'
